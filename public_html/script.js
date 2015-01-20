8d6cecf61bbf493f425fbf25605f6706ba044a9b
Esme Grundy <eg2485@gmail.com>
2015-01-20 11:29:37 -0800

    Fixed Stuff

--- public_html/script.js
+++ public_html/script.js
@@ -1,12 +1,13 @@
 $("document").ready(function() {
-    $("#intro").bind("mouseover", mouseOverMe)
+    $("#intro").bind("mouseover", mouseOverMe)//binds a hover function to the header in the jumbotron
             .bind("mouseout", mouseOutMe);
+    $("body").scrollspy({target: ".navbar"});
 });
 
-function mouseOverMe(){
-    $("#intro").html("I'm a senior in Computer Science");
+function mouseOverMe() {
+    $("#intro").html("I'm a senior in Computer Science");//when the user hovers over the intro, the text changes
 }
 
-function mouseOutMe(){
-    $("#intro").html("Click the button to find out more!");
+function mouseOutMe() {
+    $("#intro").html("Click the button to find out more!");//when the user moves away from the intro, the text changes
 }
